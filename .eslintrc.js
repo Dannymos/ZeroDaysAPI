9fa9d3ca326e41917506f989ec0e70202a5fa239
Danny Mos <dannymoswow@gmail.com>
2021-10-14 11:56:30 +0200

    Added Swagger

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'react/jsx-filename-extension': 'off'
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import AppModule from './modules/appModule';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('ZeroDays API')
+      .setDescription('The ZeroDays API description')
+      .setVersion('1.0')
+      .addTag('ZeroDays')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
